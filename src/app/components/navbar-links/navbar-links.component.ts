1b96a8bf2f6ffa07bf48cbeb84da420d96250f5a
Kiran Anand <liteninkiran@hotmail.com>
2025-05-17 16:24:33 +0100

    Add navbar

---
+++ src/app/components/navbar-links/navbar-links.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar-links',
+    templateUrl: './navbar-links.component.html',
+    styleUrls: ['./navbar-links.component.scss'],
+})
+export class NavbarLinksComponent {
+
+    public config: IConfig[] = [
+        {
+            link: '/home',
+            icon: 'home',
+            text: 'Home',
+        },
+    ]
+}
+
+interface IConfig {
+    link: string,
+    icon: string,
+    text: string,
+}
